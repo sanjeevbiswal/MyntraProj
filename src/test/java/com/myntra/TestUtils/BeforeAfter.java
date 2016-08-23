98d277f9c90ff34aad4bba5e2b2cdf0bd25d15f0
Sanjeeb <sanjeevbiswal1005@gmail.com>
2016-08-23 12:13:32 +0530

    Added screenshot method

--- src/test/java/com/myntra/TestUtils/BeforeAfter.java
+++ src/test/java/com/myntra/TestUtils/BeforeAfter.java
@@ -4,12 +4,14 @@ package com.myntra.TestUtils;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -31,8 +33,8 @@ public class BeforeAfter {
         driver.close();
     }
 
-    public static void takeScreenshot(){
+    public static void takeScreenshot(String methodname) throws IOException {
         File file= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        
+        FileUtils.copyFile(file,new File("./screenshot"+methodname+".jpg"));
     }
 }
